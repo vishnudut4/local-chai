047ccf1258de4f3e3d7afc2fde61dbbd2bc1e713
vishnudut4 <vishnudut4@gmail.com>
2026-06-12 22:00:34 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const { Pool } = require('pg');
+require('dotenv').config();
 
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+});
+
+app.get('/', (req, res) => {
+  res.send('Chaiwala API Running');
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Server Started');
+});
